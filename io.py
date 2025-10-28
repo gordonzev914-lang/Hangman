a23df78328a7aab2647962f2d8815f368992b696
gordonzev914-lang <gordonzev914@gmail.com>
2025-10-28 18:04:14 +0200

    Add files via upload

---
+++ io.py
@@ -0,0 +1,15 @@
+# import words
+def prompt_guess():
+    player_word=input("enter a word")
+    return {"player_word":player_word}
+
+
+
+def print_status(state: dict):
+    pass
+
+
+def print_result(state: dict):
+    pass
+
+
